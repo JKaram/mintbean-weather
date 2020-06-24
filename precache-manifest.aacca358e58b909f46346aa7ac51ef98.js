2b62f63f863de0d8e60c2ee46a2587957295eaa4
jkaram <jamiekaram16@gmail.com>
2020-06-24 11:50:46 -0400

    Updates

---
+++ precache-manifest.aacca358e58b909f46346aa7ac51ef98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf955514dc9b85e13d14294652e270d5",
+    "url": "/mintbean-weather/index.html"
+  },
+  {
+    "revision": "0db86deb4fa7679e3aa2",
+    "url": "/mintbean-weather/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3fdf9e9133d870320055",
+    "url": "/mintbean-weather/static/js/2.b3d146c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mintbean-weather/static/js/2.b3d146c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db86deb4fa7679e3aa2",
+    "url": "/mintbean-weather/static/js/main.4b4bf98c.chunk.js"
+  },
+  {
+    "revision": "a828fb7502ee34949c46",
+    "url": "/mintbean-weather/static/js/runtime-main.a0963937.js"
+  }
+]);
